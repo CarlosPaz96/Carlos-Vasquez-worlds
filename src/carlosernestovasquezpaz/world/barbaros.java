e17366e564ff6542d18ea56ff6541aa4a01c6e51
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2018-06-13 15:58:21 -0600

    Create barbaros.java

---
+++ src/carlosernestovasquezpaz/world/barbaros.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlosernestovasquezpaz.world;
+
+/**
+ *
+ * @author carlosPaz
+ */
+public class barbaros implements raza{
+
+    @Override
+    public String nombre() {
+        int hola;
+        hola = poder();
+           return "BARBAROS"+hola;
+    }
+    
+    public int poder(){
+        return 10;
+    }
+    
+}
