f8cf6d6d59a37923310e1e0ab31a1df063542565
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2018-06-13 15:56:06 -0600

    Create Validaciones.java

---
+++ src/carlosernestovasquezpaz/world/Validaciones.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlosernestovasquezpaz.world;
+
+import static carlosernestovasquezpaz.world.menusDeOpciones.MenuDGuerra;
+
+/**
+ *
+ * @author rafa
+ */
+public class Validaciones {
+
+    public Validaciones() {
+    }
+    
+    public static int menuGerra(int op,String nombre,int oro,int elipsis,int madera){
+        if(op<1 && op>5 ){
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("|La opcion es incorrecta, porfavor escoja entre las opciones 1,2,3.|");
+            System.out.println("--------------------------------------------------------------------");
+            MenuDGuerra(nombre,oro,elipsis,madera);
+        }
+        return op;
+    }
+}
