887614bb4b3813d977f3dfb21b2a6bc7e97d04e4
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2018-06-15 11:51:35 -0600

    Create minasDeoro.java

---
+++ src/carlosernestovasquezpaz/world/minasDeoro.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlosernestovasquezpaz.world;
+
+/**
+ *
+ * @author CarlosPaz
+ */
+public class minasDeoro implements edificio {
+
+    @Override
+    public String nombre(String raza) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int nivel(int nivel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int cont(int recurso) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int vida() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
