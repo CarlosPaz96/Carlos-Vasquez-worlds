16738b961241290ff6fec4bcdfa7dc1a94ac8bfd
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2018-06-15 11:49:25 -0600

    Create edificio.java

---
+++ src/carlosernestovasquezpaz/world/edificio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlosernestovasquezpaz.world;
+
+/**
+ *
+ * @author CarlosPaz
+ */
+public interface edificio {
+    public String nombre(String raza);
+    public int nivel(int nivel);
+    public int cont(int recurso);
+    public int vida();
+}
