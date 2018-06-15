948c09240ef9a12aaf20be2dd8c50e50ea101bcb
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2018-06-15 11:47:39 -0600

    Update Validaciones.java

--- src/carlosernestovasquezpaz/world/Validaciones.java
+++ src/carlosernestovasquezpaz/world/Validaciones.java
@@ -9,20 +9,31 @@ import static carlosernestovasquezpaz.world.menusDeOpciones.MenuDGuerra;
 
 /**
  *
- * @author rafa
+ * @author CarlosPaz
  */
+
+import java.util.*;
+
 public class Validaciones {
+
+    private static int MenuDGuerra(String nombre, int oro, int elipsis, int madera, String razaDdue, ArrayList oponente, ArrayList yo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     public Validaciones() {
     }
     
-    public static int menuGerra(int op,String nombre,int oro,int elipsis,int madera){
-        if(op<1 && op>5 ){
-            System.out.println("--------------------------------------------------------------------");
-            System.out.println("|La opcion es incorrecta, porfavor escoja entre las opciones 1,2,3.|");
-            System.out.println("--------------------------------------------------------------------");
-            MenuDGuerra(nombre,oro,elipsis,madera);
-        }
-        return op;
+    public static int menuGerra(int op,String nombre,int oro,int elipsis,int madera,String razaDdue,ArrayList oponente,ArrayList yo){
+        if(op!=1 && op!=2 && op!=3 && op!=4 && op!=5 && op!=6 ){
+            System.out.println("------------------------------------------------------------------------");
+            System.out.println("|La opcion es incorrecta, porfavor escoja entre las opciones disponibles|");
+            System.out.println("------------------------------------------------------------------------");
+            int validar=MenuDGuerra(nombre,oro,elipsis,madera,razaDdue,oponente,yo);
+            return op;
     }
+        return op;
+}
+    
+
+
 }
