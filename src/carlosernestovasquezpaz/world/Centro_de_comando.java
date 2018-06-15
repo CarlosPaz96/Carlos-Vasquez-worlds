1f705e501fa61bf3b2ff9df6e335f1668c28b109
CarlosPaz96 <38087007+CarlosPaz96@users.noreply.github.com>
2018-06-15 11:44:39 -0600

    Create Centro_de_comando.java

---
+++ src/carlosernestovasquezpaz/world/Centro_de_comando.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlosernestovasquezpaz.world;
+
+/**
+ *
+ * @author CarlosPaz
+ */
+public class Centro_de_comando {
+    public String duenno;
+    public String razaDeDuenno;
+    public int vida;
+    public int oro;
+    public int madera;
+    public int elixir;
+    
+    Centro_de_comando(){}
+
+    public Centro_de_comando(String duenno) {
+        this.duenno = duenno;
+    }
+    
+    
+    
+}
